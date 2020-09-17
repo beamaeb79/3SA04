96f442df883b5af8246615ded6bf9d0d16169ca6
beamaeb79 <s6135512010@phuket.psu.ac.th>
2020-09-17 11:54:30 +0700

    second commit

--- components/Weather.js
+++ components/Weather.js
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, ImageBackground, StyleSheet } from 'react-native';
 import Forecast from './Forecast';
 
 export default function Weather(props) {
+    const apiKey = 'd27c486fa449cd2fa90a29db10f0ea94'
     const [forecastInfo, setForecastInfo] = useState({
         main: 'Main',
         description: 'Description',
         temp: 0,
     })
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {              
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=${apiKey}`)
+                   .then((response) => response.json())
+                   .then((json) => {
+                       setForecastInfo({
+                           main: json.weather[0].main,
+                           description: json.weather[0].description,    
+                           temp: json.main.temp
+                       });
+                   })
+                   .catch((error) => {
+                       console.warn(error);
+                   });
+           }
+       }, [props.zipCode])
+   
     return (
         <View>
             <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
@@ -26,7 +46,7 @@ const styles = StyleSheet.create({
         height: '100%',
     },
     cover: {
-        backgroundColor: 'grey', 
+        backgroundColor: '#000', 
         width: '100%',
         height: 300,
         opacity: 0.5,
